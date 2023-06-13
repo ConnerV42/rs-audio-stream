0b71e502989a1da637fac1acb4faec5e52c62de5
Conner_Verret <connerverret@protonmail.com>
2023-06-12 21:51:39 -0700

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,13 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
-
-#[derive(serde::Deserialize)] struct FormData {
+#[derive(serde::Deserialize)]
+struct FormData {
     email: String,
-    name: String
+    name: String,
 }
 
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse { 
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
@@ -17,7 +17,7 @@ async fn health_check() -> HttpResponse {
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(||  {
+    let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
